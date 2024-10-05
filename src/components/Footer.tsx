b593046b93f4834742f63d28f51e6560a24ed35e
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2024-10-05 21:48:37 +0100

    Added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
+import logo from "/assets/images/logo.png";
+import facebookIcon from "/assets/icons/facebook.svg";
+import twitterIcon from "/assets/icons/twitter.svg";
+import instagramIcon from "/assets/icons/instagram.svg";
+
 const Footer = () => {
   return (
-    <footer className="bg-blue text-white">
-      <h1>Footer</h1>
+    <footer className="bg-blue text-white p-[5em] pb-[2.5em]">
+      <div className="flex flex-wrap items-center justify-between max-w-[60em] m-auto mb-[3em]">
+        <img className="w-[9.5em]" src={logo} alt="Logo" />
+        <div className="flex flex-wrap items-center justify-between gap-[1.5em]">
+          <img className="w-[1.7em]" src={facebookIcon} alt="Facebook Icon" />
+          <img className="w-[1.7em]" src={twitterIcon} alt="Twitter Icon" />
+          <img className="w-[1.7em]" src={instagramIcon} alt="Instagram Icon" />
+        </div>
+      </div>
+      <p className="font-[100] text-[0.8em] text-center tracking-[0.1em]">
+        ©smileschool 2020
+      </p>
     </footer>
   );
 };

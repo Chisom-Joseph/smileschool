aa5699093ceab3ddedb32916cb26da80d361bd31
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2024-10-05 22:25:00 +0100

    Styed ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <main>
-      <h1>Error</h1>
-      <p>Something went wrong</p>
-    </main>
+    <section className="text-white text-center grid content-center bg-blue h-[100dvh] leading-[7em]">
+      <div>
+        <h1 className="font-[1000] text-[10em]">Error</h1>
+        <p className="font-[100] text-[2em]">Something went wrong</p>
+        <Link className="btn" to="/">
+          Go home
+        </Link>
+      </div>
+    </section>
   );
 };
 

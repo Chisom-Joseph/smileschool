4effda013341d37591ff28027a8d04053c2dee4b
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2024-10-14 08:49:54 +0100

    Fixed testimonials section responsiveness

--- src/components/TestimonialsSection.tsx
+++ src/components/TestimonialsSection.tsx
@@ -2,17 +2,17 @@ import testimonialImage from "/assets/images/testimonialImage.png";
 
 const TestimonialsSection = () => {
   return (
-    <section className="text-[1.1em] text-left bg-purple text-white grid place-content-center p-[5em] tracking-[0.05em]">
-      <div className="flex items-center  max-w-[50em]">
-        <div className="rounded-[50%] overflow-hidden w-fit">
+    <section className="grid place-content-center bg-purple p-[5em] text-left text-[1.1em] tracking-[0.05em] text-white">
+      <div className="flex max-w-[50em] flex-wrap items-center justify-center gap-[3em]">
+        <div className="w-fit min-w-[8em] overflow-hidden rounded-[50%]">
           <img
             className="w-[11.5em]"
             src={testimonialImage}
             alt="Testimonial image"
           />
         </div>
-        <div className="ml-[3em]">
-          <p className="font-[200] mb-[1.5em]">
+        <div className="max-w-[35em]">
+          <p className="mb-[1.5em] font-[200]">
             « Those tutorials are concise and go straight to the point. I can’t
             think of a better place to learn smiling. And it’s so fun!
           </p>
